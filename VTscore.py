2ce43fb667bed0b30e265a31e9ed93f0ffc44669
ElielLopez <elielsw1@gmail.com>
2022-03-01 16:16:37 +0200

    adding comments. writing read me

--- VTscore.py
+++ VTscore.py
@@ -1,7 +1,8 @@
 import requests
 import argparse
 
-# this function checks if the API key does not contain 64 characters its not valid.
+
+# this function checks if the API key is valid (contain 64 characters) or not.
 def check_key(api_key):
     if len(api_key) == 64:
         print("Valid key\n")
@@ -11,7 +12,7 @@ def check_key(api_key):
         return False
 
 
-# this function checks if the file exist inside the directory, if not it will exit the program.
+# this function checks if the file exist inside the directory, if not it will print error.
 def check_file(file_name):
     try:
         f = open(file_name)
@@ -24,35 +25,38 @@ def check_file(file_name):
 
 
 def analyze_sha1(key, hash):
-    print(key +"\n"+ hash)
     parameters = {"apikey": key, "resource": hash}
     url = requests.get("https://www.virustotal.com/vtapi/v2/file/report", params=parameters)
     j_resp = url.json()
     response = int(j_resp.get("response_code"))
 
     if response == 0:
-        print("Unknown\n")
+        print("Hash file not found on Virus Total data base\n")
     elif response == 1:
         positives = int(j_resp.get("positives"))
         if positives >= 50:
             print("Malicious\n")
         else:
-            print("Not malicious\n")
+            print("Not Malicious\n")
 
+
+# using VT API, this function will checks a certain hash file (specifically SHA-1) on Virus Total
+# data base and returns if the hash file is malicious (over 50 engines detects it as malicious) or not.
 if __name__ == '__main__':
-    # api key: 8abe0b4cd868be301d050be4dcfef73f34f41092063118f236f9cd203f9d0e54
+    # two arguments are required to run this program: Valid API key and path to file
     parser = argparse.ArgumentParser()
     parser.add_argument("-k", "--api_key", required=True)
     parser.add_argument("-i", "--input", required=True)
     args = parser.parse_args()
 
     if check_key(args.api_key) and check_file(args.input):
-        print("YAS")
+        print("Proccessing\n")
         f = open(args.input, "r")
         # this section can handles file with one SHA-1 and can be transform to handles file with multiple SHA-1.
         sha1_input = f.readline()
 
         analyze_sha1(args.api_key, sha1_input)
 
+    # if the api key or the file are invalid an error will be printed
     else:
-        print("NOOO")
+        print("The key or hash file you provided are invalid")
