2704c7be9284aeef7eeb62a22660009b7242bf37
ElielLopez <elielsw1@gmail.com>
2022-03-02 13:07:32 +0200

    parsing rss feed with pandas (first try with feedparser, still in comments), need to understand why getting duplicate lines

--- CPfeed.py
+++ CPfeed.py
@@ -1,12 +1,103 @@
 import flask
 import requests
 import urllib3
-
+import feedparser
+import re
+from bs4 import BeautifulSoup
+import requests
+import pandas
+from requests_html import HTML
+from requests_html import HTMLSession
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+# urllib3.disable_warnings()
+# url = 'https://research.checkpoint.com/feed/'
+# headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+# response = requests.post(url, headers=headers)
+# print(response.status_code)
+# print(response.text)
 # url = "https://research.checkpoint.com/feed/"
+
+
+# get feed funtsion recieved url of rss feed and return the feed content
+def get_feed(url):
+    response = get_source(url)
+    data_frame = pandas.DataFrame(columns=['title', 'pubDate', 'guid', 'description'])
+
+    with response as r:
+        items = r.html.find("item", first=False)
+
+        for item in items:
+            title = item.find('title', first=True).text
+            pubDate = item.find('pubDate', first=True).text
+            guid = item.find('guid', first=True).text
+            description = item.find('description', first=True).text
+
+            row = {'title': title, 'pubDate': pubDate, 'guid': guid, 'description': description}
+            data_frame = data_frame.append(row, ignore_index=True)
+
+    return data_frame
+
+
+# get source receives url to rss feed and return the source code (http)
+def get_source(url):
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+
 if __name__ == '__main__':
-    urllib3.disable_warnings()
-    url = 'https://research.checkpoint.com/feed/'
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-    response = requests.post(url, headers=headers)
-    print(response.status_code)
-    print(response.text)
+    url = "https://research.checkpoint.com/feed/"
+    df = get_feed(url)
+    df.head()
+
+    print(df.head)
+
+    # NewFeed = feedparser.parse("https://research.checkpoint.com/feed/")
+    # # returns how many articles in the feed.
+    # num_of_articles = len(NewFeed.entries)
+    # print("Number of recent publication: ", num_of_articles)
+    # # getting lists of article entries
+    # articles = NewFeed.entries
+    #
+    # # dictionary for holding articles details
+    # #articles_details = {"article title": NewFeed.feed.title, "article link": NewFeed.feed.link}
+    # article_list = []
+    #
+    # # iterating over individual posts
+    # for article in articles:
+    #     temp = dict()
+    #
+    #     # if any post doesn't have information then throw error.
+    #     try:
+    #         temp["title"] = article.title
+    #         temp["author"] = article.author
+    #         temp["time_published"] = article.published
+    #         temp_summary = article.summary
+    #         # sentences = temp_summary.split('<p> <a href= </a>')
+    #         sentences = re.split("<p>", temp_summary)
+    #         temp["summary"] = sentences
+    #     except:
+    #         pass
+    #
+    #     article_list.append(temp)
+    #
+    # for a in article_list:
+    #     print(a["title"], a["author"], a["time_published"], "\n")
+    # storing lists of articles in the dictionary
+    #articles_details["articles"] = article_list
+
+
+    # entry1 = NewFeed.entries[1]
+    # print(entry1.title)
+    # print(entry1.author)
+    # print(entry1.published)
+    # print(entry1.summary)
+    # summary1 = entry1.summary
+    # sentences = summary1.split('<p>')
+    # for e in sentences:
+    #     print(e)
