09494cf7703af63cea32bf73e9a95dff6529060f
ElielLopez <elielsw1@gmail.com>
2022-03-03 12:43:23 +0200

    creating list of links. need to check every link for IOCs

--- CPfeed.py
+++ CPfeed.py
@@ -1,100 +1,79 @@
-import flask
-import requests
-import urllib3
 import feedparser
-import re
-from bs4 import BeautifulSoup
-import requests
-import pandas
-from requests_html import HTML
-from requests_html import HTMLSession
-import os.path
 from os import path
 import warnings
+import urllib3
+import requests
+import xml.etree.ElementTree as ET
+import os
+
+from bs4 import BeautifulSoup
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
-
-# urllib3.disable_warnings()
-# url = 'https://research.checkpoint.com/feed/'
-# headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-# response = requests.post(url, headers=headers)  # get
-# print(response.status_code)
-# print(response.text)
-# print(response.content)
-
-
-# get feed function received url of rss feed and return the feed content
-# def get_feed(url):
-#     response = get_source(url)
-#     data_frame = pandas.DataFrame(columns=['title', 'pubDate', 'guid', 'description'])
-#
-#     with response as r:
-#         items = r.html.find("item", first=False)
-#
-#         for item in items:
-#             title = item.find('title', first=True).text
-#             pubDate = item.find('pubDate', first=True).text
-#             guid = item.find('guid', first=True).text
-#             description = item.find('description', first=True).text
-#
-#             row = {'title': title, 'pubDate': pubDate, 'guid': guid, 'description': description}
-#             data_frame = data_frame.append(row, ignore_index=True)
-#
-#     return data_frame
-#
-#
-# # get source receives url to rss feed and return the source code (http)
-# def get_source(url):
-#     try:
-#         session = HTMLSession()
-#         response = session.get(url)
-#         return response
-#
-#     except requests.exceptions.RequestException as e:
-#         print(e)
 
 
 if __name__ == '__main__':
-    # url = "https://research.checkpoint.com/feed/"
-    # d_frame = get_feed(url)
-    # d_frame.head()
-    # print(d_frame)
 
-    # NewFeed = feedparser.parse("https://research.checkpoint.com/feed/")
-    # # returns how many articles in the feed.
-    # num_of_articles = len(NewFeed.entries)
-    # print("Number of recent publication: ", num_of_articles)
-    # # getting lists of article entries
-    # articles = NewFeed.entries
-    #
-    # # dictionary for holding articles details
-    # # articles_details = {"article title": NewFeed.feed.title, "article link": NewFeed.feed.link}
-    # article_list = []
-    #
-    # # print(NewFeed.feed)
-    #
-    # # iterating over individual posts
-    # for article in articles:
-    #     temp = dict()
-    #
-    #     # if any post doesn't have information then throw error.
-    #     try:
-    #         temp["title"] = article.title
-    #         temp["author"] = article.author
-    #         temp["time_published"] = article.published
-    #         temp_summary = article.summary
-    #         # sentences = temp_summary.split('<p> <a href= </a>')
-    #         sentences = re.split("<p>", temp_summary)
-    #         temp["summary"] = sentences
-    #     except:
-    #         pass
-    #
-    #     article_list.append(temp)
-    #
-    # for a in article_list:
-    #     print(a["time_published"], a["title"], a["author"], "\n")
     url = 'https://research.checkpoint.com/feed/'
     parsed_feed = feedparser.parse(url)
 
+    # parsing xml to extract IOCS
+    urllib3.disable_warnings()
+    url = 'https://research.checkpoint.com/feed/'
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    response = requests.get(url, headers=headers)  # get
+    # print(response.status_code)
+    # print(response.text)
+
+    # content = []
+    # # Read the XML file
+    # with open("feed.xml", "r") as file:
+    #     # Read each line in the file, readlines() returns a list of lines
+    #     content = file.readlines()
+    #     # Combine the lines in the list into a string
+    #     content = "".join(content)
+    #     bs_content = BeautifulSoup(content, "lxml")
+    #
+    # result = bs_content.find("strong") #<strong>Top Attacks and Breaches</strong>
+    # #result = bs_content.find_all("strong") # [<strong>Top Attacks </strong>...<strong>Top Attacks </strong>]
+    # print(result)
+    #
+    # # h1_tag = bs_content.find_all("h1")
+    # # result2 = h1_tag.contents
+    # # print(result2)
+
+
+    # getting [] None, None
+    # # Parse XML from a file object
+    # BeautifulSoup(markup, "lxml-xml")
+    # with open("feed.xml") as file:
+    #     soup = BeautifulSoup(file, features="lxml-xml")
+    #
+    # # Parse XML from a Python string
+    # soup = BeautifulSoup("IOCs", features="lxml-xml")
+    # print(soup.prettify())
+    #
+    # print(soup.find_all("<h1>IOCs</h1>", limit=10))
+    # print(soup.find("<h1>IOCs</h1>"))
+    # print(soup.find("IOCs"))
+
+
+
+    # f = open("feed.xml", "x")
+    # f.write(response.text)
+    # with open('feed.xml') as f:
+    #     lines = f.read()
+
+    # print(lines)
+    # mytree = ET.parse('feed.xml')
+    # myroot = mytree.getroot()
+    # print(mytree)
+    # print(myroot)
+
+
+
+# ---------------------------------------------
+
+    links_list = []
     # create file for IOCs if not exist, if does exist, append IOCs tot he end of the file.
     if not path.exists("IOCs.txt"):
         f = open("IOCs.txt", "x")
@@ -111,10 +90,12 @@ if __name__ == '__main__':
                 # f = open("IOCs.txt", "a")
                 f.write(item.link)
                 f.write('\n')
-                print(item.link)
-                print(True)
+                links_list.append(item.link)
+                # print(item.link)
+                # print(True)
 
             # elif "Indicator of compromise" in content.value:
             #     print(item.link)
             #     print(True)
+    print(links_list)
     print("done")
